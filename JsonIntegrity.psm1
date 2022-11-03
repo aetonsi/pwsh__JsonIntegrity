efba9f40b57519da70f1b65b40e793add94fd209
aetonsi <18366087+aetonsi@users.noreply.github.com>
2022-11-03 15:42:00 +0100

    use Test-Json only for pwsh version -ge 7.3.0

--- JsonIntegrity.psm1
+++ JsonIntegrity.psm1
@@ -13,12 +13,15 @@ function Get-JsonIntegrity {
         $validJson = $false
     }
     else {
-        if (Get-CommandExistance "Test-Json") {
+        # Use the cmdlet Test-Json ONLY if present AND if powershell version is greater than 7.3.0
+        # pwsh versions prior to 7.3.0 do not recognize arrays and primitives as valid json, incorrectly
+        # See this issue: https://github.com/PowerShell/PowerShell/issues/11384
+        if ((Get-CommandExistance "Test-Json") -and ($host.Version.Major -ge 7) -and ($host.Version.Minor -ge 3)) {
             $validJson = $text | Test-Json 2>$null
         }
         else {
             try {
-                ConvertFrom-Json $text -ErrorAction Stop
+                ConvertFrom-Json $text -ErrorAction Stop >$null
                 $validJson = $true;
             }
             catch {
